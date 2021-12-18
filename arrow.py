dbef2c9ee3d420502c43a741fad812041dcd1706
Naoto Yoshida <y_naoto@isi.imi.i.u-tokyo.ac.jp>
2021-12-19 01:35:13 +0900

    add arrow

---
+++ arrow.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# demonstration of markers (visual-only geoms)
+# Copied from https://github.com/openai/mujoco-py/blob/master/examples/markers_demo.py
+
+import math
+import time
+import os
+import numpy as np
+from mujoco_py import load_model_from_xml, MjSim, MjViewer
+from mujoco_py.generated import const
+from scipy.spatial.transform import Rotation
+
+MODEL_XML = """
+<?xml version="1.0" ?>
+<mujoco>
+    <worldbody>
+        <body name="box" pos="0 0 0.2">
+            <geom size="0.15 0.15 0.15" type="box"/>
+            <joint axis="1 0 0" name="box:x" type="slide"/>
+            <joint axis="0 1 0" name="box:y" type="slide"/>
+        </body>
+        <body name="floor" pos="0 0 0.025">
+            <geom size="1.0 1.0 0.02" rgba="0 1 0 1" type="box"/>
+        </body>
+    </worldbody>
+</mujoco>
+"""
+
+
+def euler2mat(euler):
+    r = Rotation.from_euler('xyz', euler, degrees=False)
+    return r.as_matrix()
+
+
+model = load_model_from_xml(MODEL_XML)
+sim = MjSim(model)
+viewer = MjViewer(sim)
+step = 0
+while True:
+    t = time.time()
+    x, y = math.cos(t), math.sin(t)
+    viewer.add_marker(type=const.GEOM_ARROW,
+                      pos=np.array([0, 0, 1]),
+                      label=" ",
+                      mat=euler2mat([0, np.pi/2, t]),
+                      size=(0.1, 0.1, 2),
+                      rgba=(1, 0, 0, 0.8),
+                      emission=1)
+    viewer.render()
+
+    step += 1
+    if step > 100 and os.getenv('TESTING') is not None:
+        break
